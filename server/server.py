f47782010e401963ecd7ef2c4dbd0d2609c61734
AnastasiyaPronina <anastasiya.pronina@intel.com>
2016-04-30 02:30:47 +0300

    Code realization

--- server/server.py
+++ server/server.py
@@ -2,30 +2,28 @@
 from threading import Thread
 
 class IdeaServer:
-        def __init__(self, handler):
-                self.listener = IdeaListener()
-                self.sender = IdeaSender()
-                self.handler = handler
+        def __init__(self):
+                self.socket = socket.socket()
                 self.socket.bind(('', 9090))
+                self.socket.listen(10)
+                self.connections = []
 
-        def Listen(self):
-                self.socket.listen(1)
-                connection, addr = self.socket.accept()
-                Thread.__init__(self.listener).start(connection)
+                while True:
+                    connection, addr = self.socket.accept()
+                    self.connections.append((connection, address))
+                    start_new_thread(self.Listen, connection)
 
-        def OnMessage(self):
-                Thread.__init__(self.handler).start(message)
-                self.Broadcast(message)                
+        def Listen(self, connection):
+            while True:
+                message = connection.recv(1024)
+                if not message:
+                   break
+
+                self.Broadcast(message)
 
         def Broadcast(self, message):
-                connection.sendall(message)
+                for connection in self.connections:
+                    connection.send(message)
 
-
-class IdeaListener:
-
-    def Run(self, connection):
-        while True:
-            data = connection.recv(1024)
-            self.server.OnMessage(data)
-            if not data:
-                break
+if __name__ == "__main__":
+    idea_server = IdeaServer()
