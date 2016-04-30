5a8bd82dd0db2f67f6e8852fa5e1b4f142768834
TotalAbay <totalhustle@gmail.com>
2016-04-30 16:45:19 +0300

    Gogogog

--- js/main.js
+++ js/main.js
@@ -8,26 +8,25 @@
             var s = document.getElementsByTagName('script')[0];
             s.parentNode.insertBefore(gcse, s);
 };*/
+google.load("search", "1");
 
-function f() {
-    var customSearchControl = new google.search.CustomSearchControl();
-    customSearchControl.draw('hombre');
-    
+function onLoad() {
   // Create a Custom Search Element that uses a
   // Custom Search Engine restricted to code.google.com.
   // Change the customSearchId string to the CSE ID of
   // your own Custom Search engine.
-    //var customSearchControl = new google.search.CustomSearchControl('010883068300123991560:yplkhrsjnem');
-    //customSearchControl.enableAds(/* your publisher ID */);
+  var customSearchControl =
+    new google.search.CustomSearchControl('010883068300123991560:yplkhrsjnem');
+  //customSearchControl.enableAds(/* your publisher ID */);
 
   // Set drawing options to set the root element for the
   // search form (where you have defined a div such as
   // <div id="search-form">)
-    //var drawOptions = new google.search.DrawOptions();
-    //drawOptions.setSearchFormRoot('hombre');
+  var drawOptions = new google.search.DrawOptions();
+  //drawOptions.setSearchFormRoot('hombre');
 
   // Draw the search results in the results div
-    //customSearchControl.draw('results'/*, drawOptions*/);
-};
+  customSearchControl.draw('hombre'/*, drawOptions*/);
+}
 
-f();
+google.setOnLoadCallback(onLoad);
