e5beb2e02f8923d3e03ee8e86bdc1a06eece1780
TotalAbay <totalhustle@gmail.com>
2016-04-30 03:19:48 +0300

    Adding search declaration

--- js/idea.js
+++ js/idea.js
@@ -5,6 +5,7 @@ function Idea() {
     this.id = _id;
     _id++;
     this.text = "My Idea";
+    this.description = "This is my idea. Hooray!";
     this.priority = 0;
 }
 
---
+++ js/searcher.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function Searcher(_idea) {
+    this.idea = _idea;
+    this.searchText = _idea.text;
+    this.keyWords = _idea.description; //parse the description to get key words?
+    //TODO: sync with search engine api
+}
+
+Searcher.prototype.Start = function() {
+    //TODO: implement searching algorithm
+}
