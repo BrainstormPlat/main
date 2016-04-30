b5818147cec401fe395d1763408c02df526bc545
Elizaveta <eljogar@mail.ru>
2016-04-30 08:01:06 +0300

    edit client.js

--- js/client.js
+++ js/client.js
@@ -1,118 +1,105 @@
 //"use strict"
-$.ajaxSetup({url: '10.254.193.145:9090', type: 'post', dataType: 'json'});
+$.ajaxSetup({url: '/10.254.193.158:9090', type: 'post', dataType: 'json'});
 
 var actions = {
-    user : new Client(),
     Connect : function (params) {
         console.log('Connected.');
         console.log('Socket: '+params.socket);
-        user.socket = params.socket;
-        user.connect = true;
-        user.Read();
+        Client.socket = params.socket;
+        Client.connect = true;
+        Client.Read();
    },
    
    Disconnect : function(params) {
        console.log("Disconnected.");
    },
    
-   Print = function(params) {
+   Print : function(params) {
     condole.log("Message: "+params.message);
    } 
 };
 
-// Action.prototype.Connect = function (params) {
-//      console.log('Connected.');
-//      console.log('Socket: '+params.socket);
-//      this.user.socket = params.socket;
-//      this.user.connect = true;
-//      this.user.Read();
-// }
-
-// Action.prototype.Disconnect = function(params) {
-//     console.log("Disconnected.");
-// }
-
-// Action.prototype.Print = function(params) {
-//     condole.log("Message: "+params.message);
-// }
-
-function Client () {
-    this.socket = 0;
-    this.connect = false;
-    this.busy = false;
-    this.read = 0;
-}
-
-Client.prototype.OnSuccess =  function(data) {
-    if (typeof data.actions == 'object') {
-        for (var i = 0, iLength = data.actions.length; i < iLength; i++) {
-             if (typeof actions[data.actions[i].action] == 'function') {
-                 actions[data.actions[i].action](data.actions[i].params);
-             } 
+var Client  =  {
+    socket : 0,
+    connect : false,
+    busy : false,
+    read : 0,
+    
+    OnSuccess : function(data) {
+        if (typeof data.actions == 'object') {
+            for (var i = 0, iLength = data.actions.length; i < iLength; i++) {
+                if (typeof actions[data.actions[i].action] == 'function') {
+                    actions[data.actions[i].action](data.actions[i].params);
+                } 
+            }
+        }
+    },
+    
+    onError : function() {
+     console.log('onError');
+    },
+    
+    onComplete : function(xhr) {
+        if (xhr.status == 404) {
+            actions.Disconnect();
+        }
+        Client.busy = false;
+    },
+    
+    OnCompleteRead : function(xhr) {
+        if (xhr.status == 200) {
+            Client.Read();
+        } else 
+            setTimeout(Client.Read, 5000);
+    },
+    
+    Connect : function() {
+        if (Client.connect && Client.busy) {
+            console.log('Connecting...');
+            Client.busy = true;
+            $.ajax({
+                data: 'action=Connect',
+                success: Client.OnSuccess,
+                complete: Client.OnComplete
+            });
+        }
+    },
+    
+    Disconnect : function() {
+        if (Client.connect && Client.busy && Client.read) {
+            console.log('Disconnecting...');
+            Client.busy = true;
+            $.ajax({
+            data: 'action=Disconnect&sock='+Client.socket,
+            success: Client.OnSuccess,
+            complete: Client.OnComplete
+            });
+            Client.socket = 0;
+            Client.connect = false;
+            Client.read.abort();
+        }
+    },
+    Send : function(data) {
+         if (Client.connect) {
+            console.log("RESPONSE1");
+            $.ajax({
+                data: 'action=Send&sock='+Client.socket + '&data=' + data,
+                success: Client.OnSuccess,
+                complete: Client.OnComplete
+            });
+            console.log("RESPONSE2");
+          }
+    },
+    Read : function() {
+        if (Client.connect) {
+            Client.read = $.ajax({
+                data: 'action=Read&sock=' + Client.socket,
+                success: Client.OnSuccess,
+                complete: Client.OnCompleteRead
+            });              
         }
     }
-}
-               
-Client.prototype.onComplete = function(xhr) {
-    if (xhr.status == 404) {
-        actions.Disconnect();
-    }
-    this.busy = false;
-}     
-                  
-Client.prototype.OnCompleteRead = function(xhr) {
-    if (xhr.status == 200) {
-        this.Read();
-    } else 
-        setTimeout(this.Read, 5000);
-}
-               
-Client.prototype.Connect = function() {
-    if (this.connect && this.busy) {
-        console.log('Connecting...');
-        this.busy = true;
-        $.ajax({
-            data: 'action=Connect',
-            success: this.OnSuccess,
-            complete: this.OnComplete
-        });
-    }
-}
-
-Client.prototype.Disconnect = function() {
-    if (this.connect && this.busy && this.read) {
-        console.log('Disconnecting...');
-        this.busy = true;
-        $.ajax({
-        data: 'action=Disconnect&sock='+this.socket,
-        success: this.OnSuccess,
-        complete: this.OnComplete
-        });
-        this.socket = 0;
-        this.connect = false;
-        this.read.abort();
-    }
-}
-
-Client.prototype.Send = function() {
-    if (this.connect) {
-        $.ajax({
-            data: 'action=Send&sock='+this.socket + '&data=' + data,
-            success: this.OnSuccess,
-            complete: this.OnComplete
-        });
-    }
-}
- 
-Client.prototype.Read = function() {
-    if (this.connect) {
-        this.read = $.ajax({
-            data: 'action=Read&sock=' + this.socket,
-            success: this.OnSuccess,
-            complete: this.OnCompleteRead
-            });
-    }
-}               
- 
- var client  = new Client();
- client.Send();
+};
+                              
+            
+ Client.Send(new Idea().ConvertToJson());
