0142f10e945ccdcc3415e5ed00d4bc4b999ee2b1
geralexgit <alexandgeras@gmail.com>
2016-05-01 01:14:11 +0300

    update

--- js/view.js
+++ js/view.js
@@ -3,6 +3,9 @@
 $(document).ready(function() {
     $('#ideas').hide();
     $('.idea').draggabilly({});
+    $('#add_participant').click(function () {
+        $('#participant').after('<input type="text" placeholder="Enter participant name" class="form-control participant_cl">');
+    })
     $(".idea p, .idea h3").click(function() {
         $(this).attr('contenteditable', 'true');
         // $(this).after('<button class="save_button">save</button>');
