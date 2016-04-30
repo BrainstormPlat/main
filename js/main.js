2e7d4c16005259665f7a40c997293ac5cf69b558
TotalAbay <totalhustle@gmail.com>
2016-04-30 17:05:05 +0300

    Append child fixed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-//$("#q").append("haha");
-/*function Main() {
-            var cx = '010883068300123991560:yplkhrsjnem';
-            var gcse = document.createElement('script');
-            gcse.type = 'text/javascript';
-            gcse.async = true;
-            gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(gcse, s);
-};*/
 google.load("search", "1");
 
 function onLoad() {
@@ -15,8 +5,7 @@ function onLoad() {
   // Custom Search Engine restricted to code.google.com.
   // Change the customSearchId string to the CSE ID of
   // your own Custom Search engine.
-  var customSearchControl =
-    new google.search.CustomSearchControl('010883068300123991560:yplkhrsjnem');
+  var customSearchControl = new google.search.CustomSearchControl('010883068300123991560:yplkhrsjnem');
   //customSearchControl.enableAds(/* your publisher ID */);
 
   // Set drawing options to set the root element for the
@@ -26,6 +15,7 @@ function onLoad() {
   //drawOptions.setSearchFormRoot('hombre');
 
   // Draw the search results in the results div
+  customSearchControl.execute("haha", 1, "");
   customSearchControl.draw('hombre'/*, drawOptions*/);
 }
 
