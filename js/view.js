78da56a8ae1de78990c80b1d67c314a00a6dad2f
geralexgit <alexandgeras@gmail.com>
2016-05-01 01:28:28 +0300

    update

--- js/view.js
+++ js/view.js
@@ -3,6 +3,9 @@
 $(document).ready(function() {
     $('#ideas').hide();
     $('.idea').draggabilly({});
+    $('#add_participant').click(function () {
+        $('#participant').after('<input type="text" placeholder="Enter participant name" class="form-control participant_cl">');
+    })
     $(".idea p, .idea h3").click(function() {
         $(this).attr('contenteditable', 'true');
         // $(this).after('<button class="save_button">save</button>');
@@ -34,10 +37,10 @@ $(document).ready(function() {
     $('#add_idea').click(function() {
         $(this).before('<div class="idea idea_container">' + '<h3>header</h3>' + '<p>text</p>' + '</div>');
         $('.idea').draggabilly({});
-        $('#ideas').masonry({
+        /*$('#ideas').masonry({
             // options
             itemSelector: '.idea',
             columnWidth: 200
-        });
+        });*/
     });
 });
