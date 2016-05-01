3b0219316b652bd920c26ffbd07a17a57bd5fa4e
TotalAbay <totalhustle@gmail.com>
2016-05-01 14:49:04 +0300

    Naruto

--- js/idea.js
+++ js/idea.js
@@ -1,18 +1,15 @@
 "use strict"
 
-var _id = 0;
-function Idea() {
+function Idea(_id) {
     this.id = _id;
-    _id++;
     this.text = "My Idea";
     this.description = "This is my idea. Hooray!";
     this.rating = 5;
 }
 
-function Idea(_text, _description, /*optional*/ _rating) {
-    _priority = ( _priority !== undefined ) ? _priority : 0;
+function Idea(_id, _text, _description, /*optional*/ _rating) {
+    _rating = ( _rating !== undefined ) ? _rating : 0;
     this.id = _id;
-    _id++;
     this.text = _text;
     this.description = _description;
     this.rating = _rating;
