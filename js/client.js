77df2dca36f183a51132fbe921695a946dbf18e8
Elizaveta <eljogar@mail.ru>
2016-05-01 17:28:05 +0300

    edit

--- js/client.js
+++ js/client.js
@@ -23,6 +23,7 @@ Client.prototype.connect = function(auth_json) {
              t += ', "idea":"'+this.ideas[i]+'"';
             }
             t += '}}';
+           console.log(t);
            this.connect(t);  
          }
          else if(data.id === 'ideas_list_combined') {  
