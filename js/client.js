a9d3c3b460828798e5d92323847804ce43bb7c45
Elizaveta <eljogar@mail.ru>
2016-05-02 23:14:54 +0300

    added files

---
+++ js/client.js
@@ -0,0 +1,58 @@
+"use strict"
+function _parse (data) {
+    for(var key in data)
+    {
+        console.log("key - "+key+" data - "+data[key]);
+        for( var key1 in data[key])
+        console.log("key1 - "+key+" data - "+data[key][key1]);
+    } 
+}
+
+function Client (url) {
+    this.url = url;
+    this.ideas = {};
+}
+
+Client.prototype.connect = function(auth_json) {
+    var socket = new SockJS(this.url);
+    socket.onopen = function() {
+        socket.send(auth_json);
+    };
+    socket.onmessage =  function (event) {
+        var data = jQuery.parseJSON(event.data); 
+        _parse(data);    
+         if(data.id === "time_exceeded") {
+            var t = '{"id":"ideas_list", "content":{';
+         //console.log('length - '+Object.keys(this.ideas).length);
+            var i = 0;
+            t += '"idea'+i+'":{';
+            t += this.ideas[0].id+','+this.ideas[0].text+','+this.ideas[0].description+','+this.ideas[0].rating;
+            t += '}';
+            for (var k = 1, l = this.ideas.keys.length; i < l; i++) {
+                t += ', "idea"'+k+':{'+this.ideas[0].id+','+this.ideas[0].text+','+this.ideas[0].description+','+this.ideas[0].rating;
+                t += '}';
+            }
+            t += '}';
+            console.log("data - "+t);
+           //this.connect(t);  
+            h.connect(t);
+         }
+         if(data.id === 'ideas_list_combined') {  
+            deleteIdeas();
+            var ideas = data.content;
+            this.ideas = JSON.parse(ideas);
+            for(var key in ideas)
+                drawIdea(ideas[key].text, ideas[key].description, ideas[key].rating);
+         }
+    };
+}
+
+function authInfo(response) {
+  console.log("authentification info");
+  if (response.session) {
+    console.log('user: '+response.session.mid);
+  } else {
+    console.log('not auth');
+  }
+}
+
---
+++ js/handler.js
@@ -0,0 +1,59 @@
+// function bubbleSortMod(data) {
+//     for(var i = 0; i < data.length -1; i++)
+// }
+function Handler() {
+    this.client = new Client("http://10.240.20.158:9090/chat");
+    this.participants = [];
+    this.ratings = [];
+}
+
+Handler.prototype.UpdateIdea = function(_idea) {//common api to add/update idea
+    console.log('idea');
+    this.client.ideas[_idea.id] = _idea;
+}
+
+Handler.prototype.RemoveIdea = function(_idea) {
+    delete this.client.ideas[_idea.id];
+}
+
+Handler.prototype.SendToServer = function() {
+    var sent_str = "{";
+    for (var key in this.client.ideas)
+        sent_str += JSON.stringify(this.client.ideas[key]);
+    sent_str += "}";
+}
+
+//Common stuff
+Handler.prototype.UpdateMainForm = function(_object) {
+    var array = _object.split("&");
+    this.theme = (array[0].split("="))[1];
+    for (var i = 1, l = array.length; i < l; i++) {
+        this.participants[i] = (array[i].split("="))[1];
+    }
+    var t = '{"id":"participants_list", "content":{';
+    t += '"participant":"'+this.participants[1]+'"';
+    for (var i = 2, le = this.participants.length; i < le; i++) {
+        t += ', "participant":"'+this.participants[i]+'"';
+    }
+    t += '}}';
+    console.log(t);
+    this.client.connect(t);
+}
+
+Handler.prototype.Authenticate = function(_user) {
+    this.client.connect('{"id":"new_user", "name":"'+_user+'"}');
+}
+
+Handler.prototype.Start = function() {
+    this.client.connect('{"id":"joined"}');
+}
+
+Handler.prototype.UpdateRatings = function(_idea_id, tmp) {
+    this.client.ideas[_idea_id].rating = tmp;
+    console.log(this.client.ideas[_idea_id].rating);
+    console.log(tmp);
+}
+
+Handler.prototype.connect = function(t) {
+    this.client.connect(t);
+}
